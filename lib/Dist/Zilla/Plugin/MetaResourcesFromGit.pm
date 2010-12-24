6abd5b4aee9e388ba4f742de7efc467480dddac5
Oliver Gorwits <code@gorwits.me.uk>
2010-12-24 23:43:07 +0000

    initial version - just got to go and walk the dog then will complete POD

---
+++ lib/Dist/Zilla/Plugin/MetaResourcesFromGit.pm
@@ -0,0 +1,178 @@
+package Dist::Zilla::Plugin::MetaResourcesFromGit;
+
+use Moose;
+with 'Dist::Zilla::Role::MetaProvider';
+
+our %transform = (
+  'lc' => sub { lc shift },
+  'uc' => sub { uc shift },
+  deb  => sub { 'lib'. (lc shift) .'-perl' },
+  ''   => sub { shift },
+);
+
+use String::Formatter method_stringf => {
+    -as => '_format_string',
+    codes => {
+        a => sub { $_[0]->_github->{'account'} },
+        r => sub { $_[0]->_github->{'project'} },
+        N => sub { $transform{$_[1] || ''}->( $_[0]->name ) },
+    },
+};
+
+has name => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub { $_[0]->zilla->name },
+);
+
+has remote => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'origin',
+);
+
+has homepage => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'http://github.com/%a/%r/wiki',
+);
+
+has bugtracker_web => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'https://rt.cpan.org/Public/Dist/Display.html?Name=%N',
+);
+
+has repository_url => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'git://github.com/%a/%r.git',
+);
+
+has _github => (
+    is         => 'ro',
+    isa        => 'HashRef[Str]',
+    lazy_build => 1,
+);
+
+sub _build_github {
+    my $self = shift;
+
+    my $root = dir('.git');
+    my $ini = $root->file('config');
+
+    die "GitHubMeta: need a .git/config file, and you don't have one\n"
+        unless -e $ini;
+
+    my $fh = $ini->openr;
+    my $config = Config::INI::Reader->read_handle($fh);
+
+    my $remote = $self->remote;
+    die "GitHubMeta: no '$remote' remote found in .git/config\n"
+        unless exists $config->{qq{remote "$remote"}};
+
+    my $url = $config->{qq{remote "$remote"}}->{'url'};
+    die "GitHubMeta: no url found for remote '$remote'\n"
+        unless $url and length $url;
+
+    my ($account, $project) = ($url =~ m{[:/](.+)/(.+)\.git$});
+
+    die "GitHubMeta: no github account name found in .git/config\n"
+        unless $account and length $account;
+    die "GitHubMeta: no github repository (project) found in .git/config\n"
+        unless $project and length $project;
+
+    return { account => $account, project => $project };
+}
+
+sub BUILDARGS {
+    my ($class, @arg) = @_;
+    my %attr = ref $arg[0] ? %{ $arg[0] } : @arg;
+
+    if (exists $attr{'bugtracker.web'}) {
+        $attr{'bugtracker_web'} = delete $attr{'bugtracker.web'};
+    }
+
+    if (exists $attr{'repository.url'}) {
+        $attr{'repository_url'} = delete $attr{'repository.url'};
+    }
+
+    return \%attr;
+}
+
+sub metadata {
+    my ($self) = @_;
+
+    return {
+        homepage => _format_string($self->homepage, $self),
+        bugracker  => { web => _format_string($self->bugtracker_web, $self) },
+        repository => { url => _format_string($self->repository_url, $self) },
+    };
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+1;
+
+# ABSTRACT: Provide CPAN and GitHub.com "resource" URLs for distribution metadata
+
+=head1 SYNOPSIS
+
+In your C<dist.ini> or C<profile.ini>:
+
+ [GitHubMeta]
+
+=head1 DESCRIPTION
+
+This plugin is a drop-in replacement for L<Dist::Zilla::Plugin::MetaResources>
+for users of Git. It provides three resource links to your distribution
+metadata, based on the name of the distribution and the remote URL of the Git
+repository you are working from.
+
+The default links are equivalent to:
+
+ homepage       = http://github.com/%a/%r/wiki
+ bugtracker.web = https://rt.cpan.org/Public/Dist/Display.html?Name=%N
+ repository.url = git://github.com/%a/%r.git
+
+=head1 CONFIGURATION
+
+=head2 Plugin Options
+
+=over 4
+
+=item C<name>
+
+The name of your Perl distribution in the format used by CPAN. It defaults to
+the C<name> option you have provided in C<dist.ini>.
+
+=item C<remote>
+
+The alias of the Git remote URL from which the working repository is cloned.
+It defaults to C<origin>.
+
+=item C<homepage>
+
+
+
+=item C<bugtracker.web>
+
+=item C<repository.url>
+
+=back
+
+=head2 Formatting Options
+
+
+
+
+
+
+
+
+
+
+
+
+
